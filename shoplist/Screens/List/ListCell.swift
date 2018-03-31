f0d44b0e7e7d4c7ad95634dc6cddc64c2153d258
Krzysztof Piatkowski <kpi@trifork.com>
2018-03-31 22:43:26 +0200

    Added delete button

--- shoplist/Screens/List/ListCell.swift
+++ shoplist/Screens/List/ListCell.swift
@@ -7,17 +7,18 @@ class ListCell : UITableViewCell {
             guard let item = item else {
                 return
             }
-            self.titleLabel.text = item.storeItem.name
-            self.titleLabel.textColor = item.done ? Colors.markedDoneText : Colors.normalText
             self.numberTextField.text = String(item.amount)
             self.numberTextField.textColor = item.done ? Colors.markedDoneText : Colors.normalText
             self.seperatorLabel.textColor = item.done ? Colors.markedDoneText : Colors.normalText
+            self.titleTextField.text = item.storeItem.name
+            self.titleTextField.textColor = item.done ? Colors.markedDoneText : Colors.normalText
         }
     }
 
     private let numberTextField = UITextField()
-    private let titleLabel = UILabel()
     private let seperatorLabel = UILabel()
+    private let titleTextField = UITextField()
+    private let deleteButton = UIButton()
 
     private var swipping = false
 
@@ -30,13 +31,23 @@ class ListCell : UITableViewCell {
         numberTextField.clipsToBounds = true
         numberTextField.rightView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 0))
         numberTextField.rightViewMode = .always
-
         contentView.addSubview(numberTextField)
 
         seperatorLabel.text = "⨯"
         contentView.addSubview(seperatorLabel)
 
-        contentView.addSubview(titleLabel)
+        titleTextField.textAlignment = .left
+        titleTextField.layer.borderColor = UIColor.clear.cgColor
+        titleTextField.layer.borderWidth = 1.0
+        titleTextField.layer.cornerRadius = 5
+        titleTextField.clipsToBounds = true
+        titleTextField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 0))
+        titleTextField.leftViewMode = .always
+        contentView.addSubview(titleTextField)
+
+        deleteButton.setImage(UIImage(named: "delete"), for:UIControlState.normal)
+        deleteButton.alpha = 0
+        contentView.addSubview(deleteButton)
     }
 
     required init?(coder aDecoder: NSCoder) {
@@ -45,8 +56,13 @@ class ListCell : UITableViewCell {
     
     override func layoutSubviews() {
         numberTextField.pin.left(10).vCenter().width(45).height(35)
-        seperatorLabel.pin.right(of: numberTextField).marginLeft(5).width(10).top().bottom()
-        titleLabel.pin.right(of: seperatorLabel).marginLeft(5).right().top().bottom()
+
+        seperatorLabel.pin.after(of: numberTextField).marginLeft(5).width(10).top().bottom()
+
+        deleteButton.pin.vCenter().marginLeft(10).width(25).height(25).right(10)
+
+        titleTextField.pin.after(of: seperatorLabel).before(of: deleteButton).vCenter().marginRight(5).marginLeft(5).height(35)
+
     }
 
     override func willTransition(to state: UITableViewCellStateMask) {
@@ -59,12 +75,20 @@ class ListCell : UITableViewCell {
             return
         }
         numberTextField.isEnabled = editing
+        titleTextField.isEnabled = editing
         if animated {
             let animation = Animations.borderAnimation(is: editing)
             numberTextField.layer.add(animation, forKey: #keyPath(CALayer.borderColor))
             numberTextField.layer.borderColor = (animation.toValue as! CGColor)
+            titleTextField.layer.add(animation, forKey: #keyPath(CALayer.borderColor))
+            titleTextField.layer.borderColor = (animation.toValue as! CGColor)
+            UIView.animate(withDuration:  Metrics.animationTimeFast) {
+                self.deleteButton.alpha = editing ? 1 : 0
+            }
         } else {
             numberTextField.layer.borderColor = editing ? Colors.inputBorderColor.cgColor : Colors.clear.cgColor
+            titleTextField.layer.borderColor = editing ? Colors.inputBorderColor.cgColor : Colors.clear.cgColor
+            deleteButton.alpha = editing ? 1 : 0
         }
     }
 }
